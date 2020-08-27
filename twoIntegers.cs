b02f312e34e24728fb3d27f87e06fde3fdf8a63b
carla <cfpcarla@gmail.com>
2020-08-27 10:30:47 -0400

    Implement exercise twoIntegers

---
+++ twoIntegers.cs
@@ -0,0 +1,34 @@
+using System;
+
+class GFG
+{
+
+  // function to multiply two numbers
+  // x and y
+  static int multiply(int x, int y)
+  {
+
+    // 0 multiplied with anything gives 0
+    if (y == 0)
+      return 0;
+
+    // Add x one by one
+    if (y > 0)
+      return (x + multiply(x, y - 1));
+
+    // the case where y is negative
+    if (y < 0)
+      return -multiply(x, -y);
+
+    return -1;
+  }
+
+  // Driver code
+  public static void Main()
+  {
+
+    Console.WriteLine(multiply(5, -11));
+  }
+}
+
+
